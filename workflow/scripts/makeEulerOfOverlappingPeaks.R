7c0903a0bc15a23d226b1a3f2b0d7f1e1445c0c9
sansamcl <63733772+sansamcl@users.noreply.github.com>
2022-08-22 12:03:52 -0500

    Create makeEulerOfOverlappingPeaks.R

---
+++ workflow/scripts/makeEulerOfOverlappingPeaks.R
@@ -0,0 +1,30 @@
+library(magrittr)
+library(eulerr)
+library(GenomicRanges)
+library(ggplotify)
+library(stringr)
+
+input <- snakemake@input[[1]]
+output <- snakemake@output[[1]]
+eulerFontSize <- snakemake@params[[1]]
+eulerFills <- snakemake@params[[2]]
+
+fills <- stringr::str_split(eulerFills,pattern=",") %>% .[[1]]
+
+eulerr_options(labels = list(fontsize = eulerFontSize),
+               quantities = list(fontsize = eulerFontSize-2),
+               legend = list(fontsize = eulerFontSize, vgap = 0.01))
+EulerPlot <- GenomicRanges::mcols(input) %>%
+    as.matrix %>%
+    euler(
+      .,
+      shape="ellipse") %>%
+    plot(
+      .,
+      quantities=TRUE,
+      legend=TRUE,
+      adjust_labels=TRUE,
+      fills=fills) %>%
+    as.ggplot
+
+saveRDS(EulerPlot,output)
